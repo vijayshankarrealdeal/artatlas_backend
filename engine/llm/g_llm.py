7aa9eac0dfd8c9e5a630620b6c15bbc5c5d98b60
Vijay Shankar <vijay.shankar@msci.com>
2025-06-15 14:22:20 +0000

    gemini api

--- engine/llm/g_llm.py
+++ engine/llm/g_llm.py
@@ -18,7 +18,7 @@ def llm_generate_artwork_metadata(payload: LLMInputPayload):
     image_bytes = io.BytesIO(response.content)
     image = Image.open(image_bytes)
     client = genai.Client(
-        api_key="AIzaSyC6cggUnSifC0GhyJ6pAyd9CidngT2j8x4"
+        api_key=os.getenv("GOOGLE_GENAI_API_KEY"),
     )
     model = "gemini-2.5-flash-preview-04-17"
     contents = [image, query]
